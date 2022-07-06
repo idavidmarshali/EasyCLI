29b832bb87afeb05ca706499d7839c6e50e56b13
DavidMarshal <koroshjam14@gmail.com>
2022-07-07 01:51:38 +0430

    added position independent flag to shared libs

--- build.cmd
+++ build.cmd
@@ -6,7 +6,7 @@ echo Building EasyCLI object file..
 gcc -c -o obj/EasyCLI.o -I. -O3 -Wall -Werror -Wpedantic -Wextra src/EasyCLI.c
 echo [32mBuilding Completed![0m
 echo Compiling DLL into build/libEasyCLI.dll
-gcc -shared -o build/LibEasyCLI.dll -O3 -Wall -Werror -Wpedantic -Wextra obj/EasyCLI.o include/EasyCLI.h
+gcc -shared -fPIC -o build/LibEasyCLI.dll -O3 -Wall -Werror -Wpedantic -Wextra obj/EasyCLI.o include/EasyCLI.h
 echo Compiling STATIC into build/libEasyCLI.a
 ar.exe rcs build/libEasyCLI.a obj/EasyCLI.o
 echo [32mCompiling completed[0m
