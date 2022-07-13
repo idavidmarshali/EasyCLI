2b0c969693dd381cbeca9a7feefe8bdf77791c80
DavidMarshal <koroshjam14@gmail.com>
2022-07-13 22:53:24 +0430

    fixed minor problems and separated  static from shared

--- build.cmd
+++ build.cmd
@@ -1,18 +1,22 @@
 @echo off
 
 cls
+
 pushd "%~dp0"
-echo Building EasyCLI object file..
-gcc -c -o obj/EasyCLI.o -I. -O3 -Wall -Werror -Wpedantic -Wextra src/EasyCLI.c
-echo [32mBuilding Completed![0m
-echo Compiling DLL into build/libEasyCLI.dll
-gcc -shared -fPIC -o build/LibEasyCLI.dll -O3 -Wall -Werror -Wpedantic -Wextra obj/EasyCLI.o include/EasyCLI.h
-echo Compiling STATIC into build/libEasyCLI.a
-ar.exe rcs build/libEasyCLI.a obj/EasyCLI.o
-echo [32mCompiling completed[0m
-echo Deleting object file
+echo "Building EasyCLIShared object file.."
+gcc -c -fPIC -o obj/EasyCLIShared.o -I. -O3 -Wall -Werror -Wpedantic -Wextra src/EasyCLI.c
+echo "[32mBuilding Completed![0m"
+echo "Compiling DLL into build/libEasyCLI.dll"
+gcc -shared -fPIC -o build/LibEasyCLI.dll -O3 -Wall -Werror -Wpedantic -Wextra obj/EasyCLIShared.o
+echo "Building EasyCLIStatic object file.."
+gcc -c -o obj/EasyCLIStatic.o -I. -O3 -Wall -Werror -Wpedantic -Wextra src/EasyCLI.c
+echo "Compiling STATIC into build/libEasyCLI.a"
+ar rcs build/libEasyCLI.a obj/EasyCLIStatic.o
+echo "[32mCompiling completed[0m"
+echo "Deleting object files"
 cd obj/
 del EasyCLI.o
+del EasyCLIStatic.o EasyCLIShared.o
 echo [32mDONE![0m
 pause
 
--- build.sh
+++ build.sh
@@ -1,18 +1,21 @@
 !bin/bash
+
 clear
 
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
-echo "Building EasyCLI object file.."
-gcc -c -o obj/EasyCLI.o -I. -O3 -Wall -Werror -Wpedantic -Wextra src/EasyCLI.c
+echo "Building EasyCLIShared object file.."
+gcc -c -fPIC -o obj/EasyCLIShared.o -I. -O3 -Wall -Werror -Wpedantic -Wextra src/EasyCLI.c
 echo "[32mBuilding Completed![0m"
 echo "Compiling DLL into build/libEasyCLI.dll"
-gcc -shared -fPIC -o build/LibEasyCLI.dll -O3 -Wall -Werror -Wpedantic -Wextra obj/EasyCLI.o include/EasyCLI.h
+gcc -shared -fPIC -o build/LibEasyCLI.dll -O3 -Wall -Werror -Wpedantic -Wextra obj/EasyCLIShared.o
+echo "Building EasyCLIStatic object file.."
+gcc -c -o obj/EasyCLIStatic.o -I. -O3 -Wall -Werror -Wpedantic -Wextra src/EasyCLI.c
 echo "Compiling STATIC into build/libEasyCLI.a"
-ar.exe rcs build/libEasyCLI.a obj/EasyCLI.o
+ar rcs build/libEasyCLI.a obj/EasyCLIStatic.o
 echo "[32mCompiling completed[0m"
-echo "Deleting object file"
-rm obj/EasyCLI.o
+echo "Deleting object files"
+rm obj/EasyCLIShared.o obj/EasyCLIStatic.o
 echo "[32mDONE![0m"
 read -p "Press any key to continue ..."
 
