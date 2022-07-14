4ddbf144512ffc4274b16e86ce62270d2e99e49d
DavidMarshal <koroshjam14@gmail.com>
2022-07-15 02:20:29 +0430

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ clear
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
 echo "Building EasyCLIShared object file.."
-gcc -c -fPIC -o obj/EasyCLIShared.o -I. -O3 -Wall -Werror -Wpedantic -Wextra src/EasyCLI.c
+gcc -c -fPIC -o obj/EasyCLIShared.o -I ./include -O3 -Wall -Werror -Wpedantic -Wextra src/EasyCLI.c
 echo "[32mBuilding Completed![0m"
 echo "Compiling DLL into build/libEasyCLI.dll"
-gcc -shared -fPIC -o build/LibEasyCLI.dll -O3 -Wall -Werror -Wpedantic -Wextra obj/EasyCLIShared.o
+gcc -shared -fPIC -o build/LibEasyCLI.so -O3 -Wall -Werror -Wpedantic -Wextra obj/EasyCLIShared.o
 echo "Building EasyCLIStatic object file.."
-gcc -c -o obj/EasyCLIStatic.o -I. -O3 -Wall -Werror -Wpedantic -Wextra src/EasyCLI.c
+gcc -c -o obj/EasyCLIStatic.o -I ./include  -O3 -Wall -Werror -Wpedantic -Wextra src/EasyCLI.c
 echo "Compiling STATIC into build/libEasyCLI.a"
 ar rcs build/libEasyCLI.a obj/EasyCLIStatic.o
 echo "[32mCompiling completed[0m"
