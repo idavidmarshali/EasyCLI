c371c18d5b5df540758de1b6015819b1222d0aac
DavidMarshal <koroshjam14@gmail.com>
2022-07-15 02:39:08 +0430

    minor changes

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd "$BASEDIR"
 echo "Building EasyCLIShared object file.."
 gcc -c -fPIC -o obj/EasyCLIShared.o -I ./include -O3 -Wall -Werror -Wpedantic -Wextra src/EasyCLI.c
 echo "[32mBuilding Completed![0m"
-echo "Compiling DLL into build/libEasyCLI.dll"
+echo "Compiling SharedLib into build/libEasyCLI.so"
 gcc -shared -fPIC -o build/LibEasyCLI.so -O3 -Wall -Werror -Wpedantic -Wextra obj/EasyCLIShared.o
 echo "Building EasyCLIStatic object file.."
 gcc -c -o obj/EasyCLIStatic.o -I ./include  -O3 -Wall -Werror -Wpedantic -Wextra src/EasyCLI.c
